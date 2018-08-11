ba778c716a3a07c4dd6fe525d2c1216ff6eab5e2
mariob <mariob@localhost.localdomain>
2018-08-11 14:49:09 -0600

    started spell checker

--- Assignments/5/spellChecker.c
+++ Assignments/5/spellChecker.c
@@ -4,7 +4,103 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <queue.h>
 #include <ctype.h>
+
+  //queue struct to find lowest levenshtein numbers
+struct Queue {
+  HashLink* first;
+  HashLink* last;
+  int size = 0;
+  int cap = 5;
+}
+
+/**
+ * Add to queue
+ * Uses hashlinks to implement a queue
+ * Adds links up to capacity, then has normal
+ * queue behavior (dropping front)
+ * Input: pointer to queue and hash link key 
+*/
+void addQueue(struct Queue* Q, char* word) {
+  HashLink* newLink = 0;
+    //create new link
+  newLink = malloc(sizeof(HashLink));
+  newLink->key = word;
+  newLink->next = 0;
+  
+  if (Q->size == 0) {
+    Q->first = Q->last = newLink;
+    Q->size++;
+  } else if (Q->size < Q->cap) {
+       //add to back
+    last->next = newLink;
+    Q->size++;
+  } else {
+    last->next = newLink;
+    newLink = first->next;
+      //delete the front
+    free(first);
+    return;
+}
+
+/**
+ * Frees queue memory {
+ * Input:ptr to queue 
+*/
+void destroyQ(struct Queue* q) {
+  assert(q != 0);
+  assert(q->first != 0;
+
+  HashLink* itr = q->first;
+  HashLink* garbage = 0;
+  while (itr != 0) {
+    garbage = itr;
+    itr = itr->next;
+    free(garbage);
+  }
+  free(q);
+}
+
+/** 
+ * Print the queue
+ * */
+void printQ(struct Queue* q) {
+    HashLink* itr = q->first;
+    while (itr->next != 0) {
+      printf("%s ",itr->key);
+      itr = itr->next; 
+    }
+}
+
+/** Calculate levenshtein number for map
+ * input: map and word
+ * */
+void levenshtein(char* word) {
+  for (int i = 0; i < hashMapCapacity(map); i++) {
+    linkptr = map->table[i];
+    while (linkptr != 0) {
+     if (linkptr->value < maxVal || levenshteinQ->size < levenshteinQ->cap){
+         addQueue(levenshteinQ,linkptr->key);
+         if (linkptr->value < maxVal) {
+           maxVal = linkptr->value; 
+         }
+     }
+    }
+  }
+}
+
+/**
+ * Convert word to lower case
+ * input: an ascii null-terminated word
+**/
+void lowerCase(char* word) {
+    int idx = 0;
+    while(word[idx] != '\0') {
+        word[idx] = tolower(word[idx]); 
+        idx++;
+    }
+}
 
 /**
  * Allocates a string for the next word in the file and returns it. This string
@@ -54,7 +150,18 @@ char* nextWord(FILE* file)
  */
 void loadDictionary(FILE* file, HashMap* map)
 {
-    // FIXME: implement
+          //read a word
+    word = nextWord(file);
+    while(word) {
+            //make case insensitive
+        lowerCase(word);
+            //add the word to the map
+        hashMapPut(map,word,1);
+           //delete word
+        free(word);
+          //get next word
+        word = nextWord(file);
+    }
 }
 
 /**
@@ -67,9 +174,15 @@ void loadDictionary(FILE* file, HashMap* map)
  */
 int main(int argc, const char** argv)
 {
-    // FIXME: implement
     HashMap* map = hashMapNew(1000);
-    
+    char* word;
+    struct Queue* levenshteinQ = malloc(sizeof(struct Queue));
+    int maxVal = 0;
+    char garbage;
+    int size;
+    HashLink* linkptr = 0;
+    int valid = 1;
+
     FILE* file = fopen("dictionary.txt", "r");
     clock_t timer = clock();
     loadDictionary(file, map);
@@ -82,14 +195,64 @@ int main(int argc, const char** argv)
     while (!quit)
     {
         printf("Enter a word or \"quit\" to quit: ");
+
+          //discard what's in the input buffer
+        while((garbage = getchar()) != '\n' && garbage != EOF);
+
         scanf("%s", inputBuffer);
         
         // Implement the spell checker code here..
-        
-        if (strcmp(inputBuffer, "quit") == 0)
-        {
-            quit = 1;
+          //get clean word
+        size = 0;
+        while(inputBuffer[size] != '\n'){
+           size++;
         }
+        size++;
+        word = malloc(sizeof(char) * (size);
+        for (int i = 0; i < size; i++) {
+          word[i] = inputBuffer[i];
+            //clear the buffer
+          inputBuffer[i] = 0;
+            //validate word
+          if (word[i] < 'a' || word[i] > 'z') {
+            valid = 0;
+          }
+        }
+        if (!valid) {
+          printf("Invalid input. Only letters please.\n");
+        } else {
+              //check dictionary for word
+          if (hashMapContainsKey(map,word)) {
+            printf("The inputted word... is spelled correctly\n");
+          } else {
+            printf("The inputted word... is spelled incorrectly\n");
+
+              //traverse map calculating levenshtein
+            levenshtein(map,word);
+             
+                //add words with lowest levenshtein to queue
+            maxVal = 1000000;
+            for (int i = 0; i < hashMapCapacity(map); i++) {
+                linkptr = map->table[i];
+                while (linkptr != 0) {
+                  if (linkptr->value < maxVal || levenshteinQ->size < levenshteinQ->cap) {
+                    addQueue(levenshteinQ,linkptr->key);
+                    if (linkptr->value < maxVal) {
+                      maxVal = linkptr->value; 
+                    }
+                  }
+                }
+            }
+
+            printf("Did you mean: ");
+            printQ(levenshteinQ);
+          
+          if (strcmp(word, "quit") == 0)
+          {
+              quit = 1;
+          }
+      }
+        free(word);
     }
     
     hashMapDelete(map);
